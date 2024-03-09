aa8778d301acaebe4935fcfe889496024b141f0e
Nithin Kumar <137144466+Nithin2104p@users.noreply.github.com>
2024-03-09 13:57:53 +0530

    Add files via upload

---
+++ NewCollections/NewCollections.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import './NewCollections.css'
+import new_collections from '../Assets/new_collections'
+import Item from '../Item/Item'
+
+
+const NewCollections = () => {
+    return (
+        <div className='new-collections'>
+            <h1>NEW COLLECTIONS</h1>
+            <hr />
+            <div className="collections">
+                {new_collections.map((item, i) => {
+                    let image = <img src={item.image} alt="" />
+                    // Include the image variable here
+                    return (
+                        <div className='return'>
+                            {image}
+                            <Item key={i} id={item.id} name={item.name} new_price={<p><b> Price :₹{item.new_price}</b></p>} old_price={<p>Old price :₹{item.old_price} </p>} />
+                        </div>
+                    )
+                })}
+            </div>
+        </div>
+    )
+}
+
+export default NewCollections
